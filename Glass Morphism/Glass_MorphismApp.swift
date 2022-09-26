2a18efe024c3bfeb0a236a3c0536b11a231b5bb6
Szymon Wnuk <szymon.wnuk@icloud.com>
2022-09-26 10:48:32 +0200

    Initial Commit

---
+++ Glass Morphism/Glass_MorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glass_MorphismApp.swift
+//  Glass Morphism
+//
+//  Created by Szymon Wnuk on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Glass_MorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
